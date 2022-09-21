b3ef17d007a43cc90c28787fc793ba2d36b6b0b7
br4k3n <leonelchupitea@gmail.com>
2022-09-21 09:46:25 -0300

    segundo commit
    inicialización de Angular, primeros componentes agregados, banner, header about y experience.

--- Proyecto_Final/src/app/app.module.ts
+++ Proyecto_Final/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    BannerComponent,
+    AboutComponent,
+    ExperienceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
